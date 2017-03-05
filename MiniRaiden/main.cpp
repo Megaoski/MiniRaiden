62b6a943922e670bb96f9f021fe2e186e6bb4263
Megaoski <oscar_2596@hotmail.com>
2017-03-05 18:33:32 +0100

    Minor changes
    Modified some things for working at home

--- MiniRaiden/main.cpp
+++ MiniRaiden/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* args[])
 
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-
+	
 
 	SDL_CreateWindowAndRenderer(224, 256, 0, &window, &renderer); //crea una pantalla que sale 1 milisegundo y cierra
 	
@@ -15,38 +15,45 @@ int main(int argc, char* args[])
 
 	SDL_Texture* nave = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("nave.bmp"));
 
+	SDL_Texture* oreja = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("oreja.bmp"));
+
 	SDL_Rect r;
 
 	bool run = true;
-	while(run)
+	while (run)
 	{
 
-	for (;;)
-	 {
-		//Input
-		SDL_Event event;
-		while (SDL_PollEvent(&event))
-		{
-			if (event.type == SDL_QUIT)
-				run = false;
-
-			switch (event.type)
+		
+		
+			//Input
+			SDL_Event event;
+			while (SDL_PollEvent(&event))
 			{
+				if (event.type == SDL_QUIT)
+				{
+					run = false;
+				}
 
+				switch (event.type)
+				{
+
+				}
 			}
-		}
 
-		//Graphics
+			//Graphics
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //pinta la pantalla de negro
-		SDL_RenderClear(renderer);
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //pinta la pantalla de negro  0, 0, 0, 255
+			SDL_RenderClear(renderer);
 
-		SDL_RenderCopy(renderer, title, NULL, NULL);
-		r = { 0, 0, 84, 320 };
-		SDL_RenderCopy(renderer, nave, NULL, &r); // pintamos nave en la pantalla, la primera null es la imagen completa y la segunda null en este caso  en r, unas coordenadas especificas para mostrar en pantalla la nave
+			SDL_RenderCopy(renderer, oreja, NULL, NULL);
+
+			SDL_RenderCopy(renderer, title, NULL, NULL);
+			r = { 0, 0, 84, 320 };
+			SDL_RenderCopy(renderer, nave, NULL, &r); // pintamos nave en la pantalla, la primera null es la imagen completa y la segunda null en este caso  en r, unas coordenadas especificas para mostrar en pantalla la nave
 
 
-		SDL_RenderPresent(renderer);
+			SDL_RenderPresent(renderer);
+		
 	}
 
 	SDL_Quit();
