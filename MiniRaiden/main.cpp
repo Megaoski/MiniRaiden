9e97ba775280414ab0d143ae01379c9251d2b0f7
Megaoski <oscarll3@ttc.cat>
2017-03-08 17:46:34 +0100

    Move
    added bases to move, needs changes

--- MiniRaiden/main.cpp
+++ MiniRaiden/main.cpp
@@ -5,57 +5,86 @@ int main(int argc, char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* window;
-	SDL_Renderer* renderer;
+	SDL_Window* window = SDL_CreateWindow("Game", 400, 400, 480, 640, NULL);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_PRESENTVSYNC);
+
+
 	
 
-	SDL_CreateWindowAndRenderer(224, 256, 0, &window, &renderer); //crea una pantalla que sale 1 milisegundo y cierra
-	
 	SDL_Texture* title = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("title.bmp")); //cojemos la imagen de la carpeta y cargamos la textura
-
-	SDL_Texture* nave = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("nave.bmp"));
-
-	SDL_Texture* oreja = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("oreja.bmp"));
+    SDL_Texture* navesita = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("navesitas.bmp"));
 
 	SDL_Rect r;
+	SDL_Rect j;
+
+	int spaceship_x = 0;
+	int spaceship_y = 0;
+	int spaceship_xvel = 0;
+	int spaceship_yvel = 0;
 
 	bool run = true;
 	while (run)
 	{
 
-		
-		
-			//Input
-			SDL_Event event;
-			while (SDL_PollEvent(&event))
+
+
+		//Input
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
 			{
-				if (event.type == SDL_QUIT)
-				{
-					run = false;
-				}
-
-				switch (event.type)
-				{
-
-				}
+				run = false;
 			}
+			switch (event.type)
+			{
+			case SDL_KEYDOWN:
+				
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_LEFT:
+					spaceship_xvel = -1;
+					break;
+
+				case SDLK_RIGHT:
+					spaceship_xvel = 1;
+					break;
+
+				case SDLK_UP:
+					spaceship_yvel = -1;
+					break;
+
+				case SDLK_DOWN:
+					spaceship_yvel = 1;
+					break;
+
+	
+				}
+
+			}
+
+
 
 			//Graphics
 
 			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //pinta la pantalla de negro  0, 0, 0, 255
 			SDL_RenderClear(renderer);
 
-			SDL_RenderCopy(renderer, oreja, NULL, NULL);
 
 			SDL_RenderCopy(renderer, title, NULL, NULL);
-			r = { 0, 0, 84, 320 };
-			SDL_RenderCopy(renderer, nave, NULL, &r); // pintamos nave en la pantalla, la primera null es la imagen completa y la segunda null en este caso  en r, unas coordenadas especificas para mostrar en pantalla la nave
+			j = { spaceship_x, spaceship_y, 200, 150 };
+			SDL_RenderCopy(renderer, navesita, NULL, &j);
 
 
 			SDL_RenderPresent(renderer);
+
+		}
+
+	}
+			SDL_Quit();
+			return 0;
 		
 	}
 
-	SDL_Quit();
-	return 0;
-}
+
+
